5221028bec9ea1e8ace51e4cb10989907ce8cf65
Sathish S <s.sathishmechddesign@gmail.com>
2023-08-23 18:46:09 +0530

    config changed

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-
 // https://vitejs.dev/config/
-export default defineConfig({
-	plugins: [react()],
-	define: {
-		'process.env': {},
-	},
+export default defineConfig(() => {
+	const config = {
+		plugins: [react()],
+		base: '/weather-app/',
+	};
+
+	return config;
 });
